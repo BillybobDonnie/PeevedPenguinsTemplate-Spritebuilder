856104cda22d88d908f5fe42bf773a9a9b64162f
BillybobDonnie <don.g.firth@gmail.com>
2014-10-23 13:59:29 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Donald Firth on 2014-10-23.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
