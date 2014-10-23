fe1b2826374d6820cd4cb5516c8497abadd8d52a
BillybobDonnie <don.g.firth@gmail.com>
2014-10-23 11:58:48 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,21 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Donald Firth on 2014-10-23.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+@end
