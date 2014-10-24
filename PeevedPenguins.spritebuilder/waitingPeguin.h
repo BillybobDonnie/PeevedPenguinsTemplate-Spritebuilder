f0a5e8ff2eff121d19ad888f0a39906a046f4528
BillybobDonnie <don.g.firth@gmail.com>
2014-10-24 11:55:25 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/waitingPeguin.h
@@ -0,0 +1,13 @@
+//
+//  waitingPeguin.h
+//  PeevedPenguins
+//
+//  Created by Donald Firth on 2014-10-24.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface waitingPeguin : CCSprite
+
+@end
